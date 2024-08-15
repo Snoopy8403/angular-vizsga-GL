e8455f20360704cad95bc285db979c79c1cddd05
Snoopy8403 <77445381+Snoopy8403@users.noreply.github.com>
2024-08-15 21:15:20 +0200

    update

--- src/app/covid/covid.service.ts
+++ src/app/covid/covid.service.ts
@@ -4,6 +4,7 @@ import { Cases } from '../models/cases.model';
 import { Observable } from 'rxjs';
 import { Vaccines } from '../models/vaccines.model';
 import { environment } from '../../environments/environment';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Injectable({
   providedIn: 'root'
@@ -12,14 +13,41 @@ export class CovidService {
   private readonly BASE_URL = `${environment.covidBaseUrl}`;
 
   constructor(
-    private readonly http: HttpClient
+    private readonly http: HttpClient,
+    private readonly matSnackBar: MatSnackBar
   ) { }
 
   getCases(selectedCountry: string): Observable<Cases> {
-    return this.http.get<Cases>(`${this.BASE_URL}/cases?country=${selectedCountry}`);
+    if (this.visitCounterIncrase()) {
+      return this.http.get<Cases>(`${this.BASE_URL}/cases?country=${selectedCountry}`);      
+    }
+    else {
+      this.matSnackBar.open('Elérte a maximális letöltési számot (3)!');
+      return this.http.get<Cases>(`${this.BASE_URL}/cases?country=${selectedCountry}`);
+    }
   }
 
   getVaccines(selectedCountry: string): Observable<Vaccines> {
     return this.http.get<Vaccines>(`${this.BASE_URL}/vaccines?country=${selectedCountry}`);
+  }
+
+  visitCounterIncrase(){
+    let userData = JSON.parse(localStorage.getItem('currentUser') || '{}');
+
+    if (userData.visitCounter) {
+        if (userData.visitCounter < 2) {
+          userData.visitCounter += 1;
+          localStorage.setItem('currentUser', JSON.stringify(userData));  
+          return true;
+        }
+        else {
+          return false;
+        }
+    } 
+    else{
+      userData.visitCounter += 1;
+      localStorage.setItem('currentUser', JSON.stringify(userData));
+      return true;
+    }
   }
 }
